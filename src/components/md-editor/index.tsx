03f7eb63b1c04cfbc8932d124692ed0991760848
Maid <zhp92@outlook.com>
2022-09-19 18:11:50 +0800

    fix: fix md-editor style

--- src/components/md-editor/index.tsx
+++ src/components/md-editor/index.tsx
@@ -1,11 +1,7 @@
-import { defineComponent, onMounted, PropType, ref } from "vue"
+import { defineComponent, nextTick, onMounted, PropType, ref } from "vue"
 import "./index.css"
 
-export enum MODE {
-  EDIT = "edit",
-  EDITABLE = "editable",
-  PREVIEW = "preview"
-}
+export type EditorModeType = "edit" | "editable" | "preview";
 
 export default defineComponent({
   name: "MDEditor",
@@ -13,10 +9,6 @@ export default defineComponent({
     modelValue: {
       type: String,
       require: true,
-    },
-    height: {
-      type: Number,
-      default: 260,
     },
     leftToolbar: {
       type: String,
@@ -27,36 +19,61 @@ export default defineComponent({
       default: "preview toc sync-scroll fullscreen"
     },
     mode: {
-      type: String as PropType<MODE>,
-      default: MODE.EDIT,
+      type: String as PropType<EditorModeType>,
+      default: "edit",
     }
   },
-  emits: ['update:modelValue', 'input', 'update:mode'],
+  emits: ['update:modelValue', 'input'],
   setup(props, { emit }) {
+    const mdeditor = ref();
+
     function onInput(value: string) {
       emit('update:modelValue', value)
     }
+    const onSave = (text: any, html: any) => {
+      console.log(text, html);
+    }
+    onMounted(() => {
+      console.log(mdeditor.value);
+    })
+    const save = () => {
+      mdeditor.value.save();
+    }
+    const tiggleFullscreen = () => {
+      mdeditor.value.fullscreen = !mdeditor.value.fullscreen;
+      if (mdeditor.value.fullscreen) {
+        // TODO 新增加Toast组件 函数式
+        // alert("按esc退出全屏")
+      }
+    }
 
-    function onDoubleClick(){
-      console.log('onDoubleClick');
-      emit('update:mode', MODE.EDIT)
+    const toggleTocVisible = () => {
+      // mdeditor.value.tocVisible = !mdeditor.value.tocVisible;
+      // TODO 新增加目录组件，根据titles生成
+      console.log(mdeditor.value.titles);
+      console.log(mdeditor.value);
     }
 
     return () => (
       <>
-        <div class="border-4 border-gray-100 w-full h-fit rounded-2xl z-10 overflow-hidden" onDblclick={onDoubleClick}>
-        <v-md-editor
-          placeholder="Any thounghts..."
-          modelValue={props.modelValue}
-          onUpdate:modelValue={onInput}
-          height={props.height + "px"}
-          include-level={[1, 2, 3]}
-          left-toolbar={props.leftToolbar}
-          right-toolbar={props.rightToolbar}
-          tab-size={2}
-          mode={props.mode}
+        {/* <div class="w-full h-fit mt-2 mb-2 " > */}
+          <v-md-editor
+            ref={mdeditor}
+            placeholder="Any thounghts..."
+            modelValue={props.modelValue}
+            onUpdate:modelValue={onInput}
+            include-level={[1, 2, 3]}
+            left-toolbar={props.leftToolbar}
+            right-toolbar={props.rightToolbar}
+            tab-size={2}
+            onSave={onSave}
+            height="100%"
+            mode={props.mode}
           ></v-md-editor>
-      </div>
+          {/* <div onClick={save}>save</div>
+          <div onClick={tiggleFullscreen}>fullscreen</div>
+          <div onClick={toggleTocVisible}>toc</div> */}
+        {/* </div> */}
       </>
     )
   }
