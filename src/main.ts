0c192c79760db4512fcea88605675c0eef9770b3
Maid <zhp92@outlook.com>
2022-10-07 22:29:53 +0800

    style: format code

--- src/main.ts
+++ src/main.ts
@@ -2,32 +2,31 @@ import { createApp } from 'vue'
 import App from './App'
 import router from './router'
 
-import VMdEditor from '@kangc/v-md-editor';
-import '@kangc/v-md-editor/lib/style/base-editor.css';
-import githubTheme from '@kangc/v-md-editor/lib/theme/github.js';
-import '@kangc/v-md-editor/lib/theme/style/github.css';
+import VMdEditor from '@kangc/v-md-editor'
+import '@kangc/v-md-editor/lib/style/base-editor.css'
+import githubTheme from '@kangc/v-md-editor/lib/theme/github.js'
+import '@kangc/v-md-editor/lib/theme/style/github.css'
 // highlightjs
-import hljs from 'highlight.js';
+import hljs from 'highlight.js'
 VMdEditor.use(githubTheme, {
-  Hljs: hljs,
-});
+  Hljs: hljs
+})
 
 // align plugin
-import createAlignPlugin from '@kangc/v-md-editor/lib/plugins/align';
-VMdEditor.use(createAlignPlugin());
+import createAlignPlugin from '@kangc/v-md-editor/lib/plugins/align'
+VMdEditor.use(createAlignPlugin())
 
-import VMdPreview from '@kangc/v-md-editor/lib/preview';
-import '@kangc/v-md-editor/lib/style/preview.css';
-import '@kangc/v-md-editor/lib/theme/style/github.css';
+import VMdPreview from '@kangc/v-md-editor/lib/preview'
+import '@kangc/v-md-editor/lib/style/preview.css'
+import '@kangc/v-md-editor/lib/theme/style/github.css'
 
 // todolist plugin
-import createTodoListPlugin from '@kangc/v-md-editor/lib/plugins/todo-list/index';
-import '@kangc/v-md-editor/lib/plugins/todo-list/todo-list.css';
-VMdEditor.use(createTodoListPlugin());
+import createTodoListPlugin from '@kangc/v-md-editor/lib/plugins/todo-list/index'
+import '@kangc/v-md-editor/lib/plugins/todo-list/todo-list.css'
+VMdEditor.use(createTodoListPlugin())
 
-const app = createApp(App);
-app.use(VMdEditor);
-app.use(VMdPreview); 
-console.log(router);
-app.use(router);
+const app = createApp(App)
+app.use(VMdEditor)
+app.use(VMdPreview)
+app.use(router)
 app.mount('#app')
