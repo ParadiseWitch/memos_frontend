5fe8ef9710160ce465830540350433336d892c48
Maid <zhp92@outlook.com>
2022-10-07 22:29:10 +0800

    chore: update the login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,8 +1,15 @@
 import { defineComponent } from "vue";
+import LoginOrRegist from "../../components/login-or-regist";
+import Login_or_regist from "../../components/login-or-regist";
 
 export default defineComponent({
-  setup(){
+  setup() {
 
-    return () => <div>hello,world</div>
+    return () => (<>
+      <div class="container mx-auto relative w-full min-h-screen flex flex-row justify-start sm:justify-center items-start;">
+        <LoginOrRegist></LoginOrRegist>
+      </div>
+    </>
+    )
   }
 })
