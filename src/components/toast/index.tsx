025316d40a55ca47d2d26e3047d74d7dc24c98f3
Maid <zhp92@outlook.com>
2022-10-16 22:59:53 +0800

    refactor: useToast

--- src/components/toast/index.tsx
+++ src/components/toast/index.tsx
@@ -1,97 +1,96 @@
 import { TransitionPresets, useTransition } from "@vueuse/core";
-import { defineComponent, ref } from "vue";
+import { Ref, ref } from "vue";
+import useOutsideContainer from "../outside-container";
+import { delay } from "/@/utils/time";
 
-export default defineComponent({
-  name: "Toast",
-  setup() {
-    return () => (
-      <>
-        {toastList.value.length}
-        {toastList.value.map(t => t.jsx())}
-      </>
-    )
-  }
-})
-
-interface ToastItem {
+const ToastContainerName = "toast"
+interface Toast {
   jsx: () => JSX.Element
-  show: () => void,
-  remove: (fn: Function) => void,
+  show: (top?: number) => void,
 }
 
-export type toastType = "info" | "warn" | "danger"
-const colorMap: Record<toastType, string> = {
-  info: "bg-green-50",
-  warn: "bg-yellow-50",
-  danger: "bg-red-50"
+const toastList = ref<Toast[]>([])
+
+type toastType = "info" | "warn" | "danger"
+const colorClassMap: Record<toastType, Record<string, string>> = {
+  info: {
+    "bg": "bg-green-50",
+    "border": "border-green-200",
+  },
+  warn: {
+    "bg": "bg-yellow-50",
+    "border": "border-yellow-200"
+  },
+  danger: {
+    "bg": "bg-red-50",
+    "border": "border-red-200"
+  }
 };
 
 export interface ToastOptions {
-  type?: toastType
+  type?: toastType,
+  top?: Ref<number>,
 }
 
+const toastContainer = () => (
+  <div id="toast-container">
+    {toastList.value.map(t => t.jsx())}
+  </div>
+)
 
+export const useToast = (contentText: string, opts?: ToastOptions) => {
+  const { registContainer, hasContainer, removeContainer } = useOutsideContainer()
+  if (!hasContainer(ToastContainerName)) {
+    registContainer({ name: ToastContainerName, jsx: toastContainer })
+  }
 
-const toastList = ref<ToastItem[]>([])
-export const useToast = (contentText?: string, opts?: ToastOptions) => {
-  const toast = genToast(contentText, opts)
-  toastList.value.push(toast)
   return {
-    show() {
-      toast.show()
-      toast.remove(() => {
-        let index = toastList.value.indexOf(toast)
-        if (index > -1) {
-          toastList.value.splice(index, 1)
+    async show() {
+      const len = toastList.value.length
+      const toast = genToast(contentText, { ...opts, top: ref(300 + len * 60) })
+      toastList.value.push(toast)
+      toast.show(100 + 60 * len)
+      if (toastList.value.length != 1) return
+      await delay(2000);
+      while (1) {
+        await delay(1000);
+        toastList.value.shift();
+        toastList.value.map((t, i) => t.show(100 + i * 60))
+        if (toastList.value.length <= 0) {
+          removeContainer(ToastContainerName)
+          break;
         }
-      })
+      }
     }
   }
 }
 
-const genToast = (contentText?: string | JSX.Element, opts?: ToastOptions): ToastItem => {
-  const content = ref<string | JSX.Element>(contentText || "这里是系统提示！");
-  const type = opts?.type || "info"
-  const color = ref(colorMap[type]);
-  const visible = ref(false);
-  const topSource = ref(40);
+const genToast = (contentText: string | JSX.Element, opts: ToastOptions): Toast => {
+  const content = ref<string | JSX.Element>(contentText);
+  const type = opts.type || "info"
+  const bgColor = ref(colorClassMap[type].bg);
+  const borderColor = ref(colorClassMap[type].border);
+  const topSource = opts.top || ref(300)
   const top = useTransition(topSource, {
     duration: 1000,
     transition: TransitionPresets.easeOutQuint,
   })
 
-  const show = () => {
-    visible.value = true;
-    topSource.value = 20;
-  }
-
-  const remove = (cb: Function) => {
-    setTimeout(() => {
-      visible.value = false;
-      topSource.value = 40;
-      cb();
-    }, 3000);
-  }
-
-
-  const jsx = () => (
-    <>
-      {
-        visible.value &&
-        <div class={`border w-1/2 p-4 absolute left-1/2 rounded-lg shadow-md ${color.value}`} style={`
-          top: ${top.value}%;
+  return {
+    show(top?: number) {
+      topSource.value = top || 100;
+    },
+    jsx() {
+      return <>
+        <div class={`border ${borderColor.value} w-1/2 p-4 absolute left-1/2 rounded-lg shadow-sm ${bgColor.value}`} style={`
+          top: ${top.value}px;
           transform: translate(-50%, -50%);
           z-index: 99999;
         `}>
           <div class=" text-sm text-opacity-70 text-gray-700" > {content.value} </div>
         </div>
-      }
-    </>
-  )
-  return {
-    show,
-    remove,
-    jsx,
+      </>
+    }
   }
 }
 
