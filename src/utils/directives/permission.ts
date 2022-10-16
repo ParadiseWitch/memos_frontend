f92f2384db66b3c623843e5482a4e8375303069b
Maid <zhp92@outlook.com>
2022-10-16 22:59:41 +0800

    style: format

--- src/utils/directives/permission.ts
+++ src/utils/directives/permission.ts
@@ -1,19 +1,17 @@
-import { Directive } from "vue";
-import { useGlobalState } from "/@/stage";
-
+import { Directive } from 'vue'
+import { useGlobalState } from '/@/stage'
 
 export const Permission: Directive<Element, string[]> = {
-  mounted(el, binding) {
-    const { value } = binding;
-    const { userRole } = useGlobalState();
-    console.log(value);
+  mounted (el, binding) {
+    const { value } = binding
+    const { userRole } = useGlobalState()
 
     if (!value || !(value instanceof Array) || value.length <= 0) {
-      throw new Error(`need roles! Like v-permission="['admin','user']`);
+      throw new Error(`need roles! Like v-permission="['admin','user']`)
     }
 
-    if (value.includes(userRole.value)) return;
+    if (value.includes(userRole.value)) return
     // el.style.display = 'none';
-    el.remove();
+    el.remove()
   }
 }
